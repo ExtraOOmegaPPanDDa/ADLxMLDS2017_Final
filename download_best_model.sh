08d91de420e454557aa737476a0d626352d10d7c
OOmegaPPanDDa <b02705027@ntu.edu.tw>
2018-01-18 15:00:26 +0800

    download_best_model.sh

---
+++ download_best_model.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/rn4sszduyzknqk9/hand_exist_model.h5?dl=1 -O hand_exist_model.h5
+wget https://www.dropbox.com/s/vpjuwor6tbl2woh/hand_bbx_model.h5?dl=1 -O hand_bbx_model.h5
